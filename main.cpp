bd292d2d3e4e8021ee8060f8ebe25e9653e10a7b
PETEPEtrek <senatoroff.p@gmail.com>
2021-10-24 21:13:57 +0300

    rebuild

--- main.cpp
+++ main.cpp
@@ -1,66 +1,58 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <set>
 #include <queue>
+#include <set>
+#include <vector>
 enum { INF = 2147483647 };
+enum weight { UNWEIGHTED_EDGE = 1 };
 class Graph {
 public:
     typedef size_t Vertex;
+    virtual void Add_Edge(Vertex first_vertex, Vertex second_vertex) = 0;
+    virtual std::vector<Vertex> Get_Neighbors(Vertex v) const = 0;
+    Vertex Get_Num_Of_Verteses() const {
+        return num_of_verteses;
+    }
+protected:
     bool IsDirected = false;
-    Vertex num_of_edges = 0;
+    int32_t num_of_edges = 0;
     Vertex num_of_verteses = 0;
-    virtual void Add_Edge(Vertex a, Vertex b) = 0;
-    virtual std::set<Vertex> Get_Neighbors(Vertex v) const = 0;
 };
 class AdjList : public Graph {
-    std::unordered_map<Vertex, std::set<Vertex>> list_of_adjacency;
+    std::vector<std::vector<Vertex>> list_of_adjacency;
 
-    std::set<Vertex> Get_Neighbors(Vertex v) const override {
-        if (!list_of_adjacency.count(v)) {
-            std::set<Vertex> empty_set = {};
-            return empty_set;
-        }
-        return list_of_adjacency.at(v);
+    std::vector<Vertex> Get_Neighbors(Vertex v) const override {
+        return list_of_adjacency[v];
     }
+
 public:
-    AdjList(Vertex verteses, Vertex edges) {
+    AdjList(Vertex verteses, int32_t number_of_edges) {
         num_of_verteses = verteses;
-        num_of_edges = edges;
+        num_of_edges = number_of_edges;
+        list_of_adjacency.resize(num_of_verteses + 1);
     }
-    void Add_Edge(Vertex a, Vertex b) override {
-        list_of_adjacency[a].emplace(b);
+    void Add_Edge(Vertex first_vertex, Vertex second_vertex) override {
+        list_of_adjacency[first_vertex].push_back(second_vertex);
         if (!IsDirected) {
-            list_of_adjacency[b].emplace(a);
-        }
-    }
-    AdjList(Vertex verteses, Vertex edges, std::vector<std::pair<Vertex, Vertex>>& graph) {
-        num_of_verteses = verteses;
-        num_of_edges = edges;
-        for (auto& item: graph) {
-            list_of_adjacency[item.first].emplace(item.second);
-            if (!IsDirected) {
-                list_of_adjacency[item.second].emplace(item.first);
-            }
+            list_of_adjacency[second_vertex].push_back(first_vertex);
         }
     }
     ~AdjList() = default;
-
 };
 
 class AdjMatrix : public Graph {
     std::vector<std::vector<Vertex>> matrix_of_adjacency;
-    std::set<Vertex> Get_Neighbors(Vertex v) const override {
-        std::set<Vertex> neighbors;
-        for (int32_t i = 1; i <= num_of_verteses; ++i) {
+    std::vector<Vertex> Get_Neighbors(Vertex v) const override {
+        std::vector<Vertex> neighbors;
+        for (Vertex i = 1; i <= num_of_verteses; ++i) {
             if (matrix_of_adjacency[v - 1][i - 1]) {
-                neighbors.emplace(i);
+                neighbors.push_back(i);
             }
         }
         return neighbors;
     }
+
 public:
-    AdjMatrix(Vertex verteses, Vertex edges) {
+    AdjMatrix(Vertex verteses, int32_t edges) {
         num_of_verteses = verteses;
         num_of_edges = edges;
         matrix_of_adjacency.resize(num_of_verteses);
@@ -68,34 +60,32 @@ public:
             line.resize(num_of_verteses, 0);
         }
     }
-    void Add_Edge(Vertex a, Vertex b) override {
-        matrix_of_adjacency[a - 1][b - 1] = 1;
+    void Add_Edge(Vertex first_vertex, Vertex second_vertex) override {
+        matrix_of_adjacency[first_vertex - 1][second_vertex - 1] = UNWEIGHTED_EDGE;
         if (!IsDirected) {
-            matrix_of_adjacency[b - 1][a - 1] = 1;
-        }
-    }
-    AdjMatrix(Vertex verteses, Vertex edges, std::vector<std::pair<Vertex, Vertex>>& graph) {
-        num_of_verteses = verteses;
-        num_of_edges = edges;
-        matrix_of_adjacency.resize(num_of_verteses);
-        for (auto& line: matrix_of_adjacency) {
-            line.resize(num_of_verteses, 0);
-        }
-        for (auto& item: graph) {
-            matrix_of_adjacency[item.first - 1][item.second - 1] = 1;
-            if (!IsDirected) {
-                matrix_of_adjacency[item.second - 1][item.first - 1] = 1;
-            }
+            matrix_of_adjacency[second_vertex - 1][first_vertex - 1] = UNWEIGHTED_EDGE;
         }
     }
     ~AdjMatrix() = default;
 };
-
-void Shortest_Path_Between_Verteses(const Graph& graph, Graph::Vertex from, Graph::Vertex to) {
-    std::vector<Graph::Vertex> distance(graph.num_of_verteses + 1, INF);
-    std::vector<Graph::Vertex> parent(graph.num_of_verteses + 1, 0);
+std::vector<Graph::Vertex> Answer_And_Making_Path(std::vector<Graph::Vertex>& parent,
+                                                  std::vector<Graph::Vertex>& distance,
+                                                  Graph::Vertex to) {
+    std::cout << distance[to] << std::endl;
+    std::vector<Graph::Vertex> answer(1, to);
+    Graph::Vertex vertex = parent[to];
+    while (vertex != 0) {
+        answer.push_back(vertex);
+        vertex = parent[vertex];
+    }
+    return answer;
+}
+std::vector<Graph::Vertex> Shortest_Path_Between_Verteses(const Graph& graph, Graph::Vertex from,
+                                                          Graph::Vertex to) {
+    std::vector<Graph::Vertex> distance(graph.Get_Num_Of_Verteses() + 1, INF);
+    std::vector<Graph::Vertex> parent(graph.Get_Num_Of_Verteses() + 1, 0);
     std::queue<Graph::Vertex> queue;
-    std::vector<bool> used(graph.num_of_verteses + 1, false);
+    std::vector<bool> used(graph.Get_Num_Of_Verteses() + 1, false);
     queue.push(from);
     used[from] = true;
     distance[from] = 0;
@@ -115,19 +105,10 @@ void Shortest_Path_Between_Verteses(const Graph& graph, Graph::Vertex from, Grap
         }
     }
     if (distance[to] == INF) {
-        std::cout << -1;
+        return {};
     } else {
-        std::cout << distance[to] << std::endl;
-        std::vector<Graph::Vertex> answer(1, to);
-        Graph::Vertex vertex = parent[to];
-        while (vertex != 0) {
-            answer.push_back(vertex);
-            vertex = parent[vertex];
-        }
-        for (size_t i = answer.size() - 1; i > 0; --i) {
-            std::cout << answer[i] << " ";
-        }
-        std::cout << answer[0];
+        std::vector<Graph::Vertex> answer = Answer_And_Making_Path(parent, distance, to);
+        return answer;
     }
 }
 
@@ -142,6 +123,14 @@ int main() {
         std::cin >> first_vertex >> second_vertex;
         graph.Add_Edge(first_vertex, second_vertex);
     }
-    Shortest_Path_Between_Verteses(graph, from, to);
+    std::vector<Graph::Vertex> answer = Shortest_Path_Between_Verteses(graph, from, to);
+    if (answer.empty()) {
+        std::cout << -1;
+    } else {
+        for (size_t i = answer.size() - 1; i > 0; --i) {
+            std::cout << answer[i] << " ";
+        }
+        std::cout << answer[0];
+    }
     return 0;
 }
